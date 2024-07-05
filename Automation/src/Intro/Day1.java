d24e6dda426fb9e05644a9448f348fda9cd14627
Pallav-cmd <pallavivarmao12@gmail.com>
2024-07-05 11:55:51 +0530

    Commiting from Gitstuff local repo

--- Automation/src/Intro/Day1.java
+++ Automation/src/Intro/Day1.java
@@ -8,6 +8,10 @@ public class Day1 {
 	public void Demo()
 	{
 		System.out.println("Hello");
+		System.out.println("Hello2");
+		System.out.println("Hello3");
+		System.out.println("Hello4");
+		System.out.println("Hello5");
 	}
 
 }
