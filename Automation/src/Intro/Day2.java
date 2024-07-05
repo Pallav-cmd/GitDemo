b2fb8300031e4317f0915116b2d047a6922c0a3a
Pallav-cmd <pallavivarmao12@gmail.com>
2024-07-05 16:33:49 +0530

    Commiting from GitDemo by adding day2 file

---
+++ Automation/src/Intro/Day2.java
@@ -0,0 +1,17 @@
+package Intro;
+
+import org.testng.annotations.Test;
+
+public class Day2 {
+	
+	@Test
+	public void Demo()
+	{
+		System.out.println("World");
+		System.out.println("World2");
+		System.out.println("World3");
+		System.out.println("World4");
+		System.out.println("World5");
+	}
+
+}
